3d91044a8465313675f56cd5f1da19834b3cbd53
Subhrajyoti <subha.ionic3@gmail.com>
2020-09-26 20:17:22 +0530

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Subhrajyoti Chakraborty on 26/09/20.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
